349c8f823577703af0f9bb72a2db599ac47d07e4
Georg Heiler <georg.kf.heiler@gmail.com>
2022-03-06 11:21:41 +0100

    update repo - still failing with: dagster.core.errors.DagsterInvalidDefinitionError: Input asset '["activity_daily_stats"]' for asset 'activity_forecast' is not produced by any of the provided asset ops and is not one of the provided sources

--- hacker_news_assets/hacker_news_assets/repo.py
+++ hacker_news_assets/hacker_news_assets/repo.py
@@ -8,11 +8,26 @@ from .jobs.hacker_news_api_download import (
  #   download_staging_job,
 )
 from .jobs.story_recommender import story_recommender_local_job#, story_recommender_prod_job, story_recommender_staging_job
-#from .sensors.hn_tables_updated_sensor import make_hn_tables_updated_sensor
-#from .sensors.slack_on_failure_sensor import make_slack_on_failure_sensor
 
+# TODO: are these required?
+# I thought that the SDA would auto-magically propagate updates
+# are these a relict from the pre SDA times?
+from .sensors.hn_tables_updated_sensor import make_hn_tables_updated_sensor
+from .sensors.slack_on_failure_sensor import make_slack_on_failure_sensor
+
+
+#@repository
+#def hacker_news_assets_local():
+#    # TODO add missing
+#    return [local_assets, download_local_job, activity_stats_local_job, story_recommender_local_job]
 
 @repository
 def hacker_news_assets_local():
-    # TODO add missing
-    return [local_assets, download_local_job, activity_stats_local_job, story_recommender_local_job]
+    return [
+        local_assets, 
+        schedule_from_partitions(download_local_job),
+        #make_slack_on_failure_sensor(base_url="my_dagit_url.com"),
+        make_hn_tables_updated_sensor(activity_stats_local_job),
+        make_hn_tables_updated_sensor(story_recommender_local_job),
+    
+    ]
