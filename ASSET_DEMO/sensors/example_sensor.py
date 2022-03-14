ebfd4a6b9986b8fd394bf7a04910b6e3f61a1b10
Georg Heiler <georg.kf.heiler@gmail.com>
2022-03-14 18:33:08 +0100

    fix sensor

--- ASSET_DEMO/sensors/example_sensor.py
+++ ASSET_DEMO/sensors/example_sensor.py
@@ -46,45 +46,6 @@ def my_directory_sensor():
 # Example remote SFTP sensor
 # spin up the docker-compose file
 # based on: https://gist.github.com/lkluft/ddda28208f7658d93f8238ad88bd45f2
-# def paramiko_glob(path, pattern, sftp):
-#     """Search recursively for files matching a given pattern.
-    
-#     Parameters:
-#         path (str): Path to directory on remote machine.
-#         pattern (str): Python re [0] pattern for filenames.
-#         sftp (SFTPClient): paramiko SFTPClient.
-        
-#     [0] https://docs.python.org/2/library/re.html
-        
-#     """
-#     p = re.compile(pattern)
-#     root = sftp.listdir(path)
-#     file_list = []
-    
-#     # Loop over all entries in given path...
-#     for f in (os.path.join(path, entry) for entry in root):
-#         f_stat = sftp.stat(f)
-#         # ... if it is a directory call paramiko_glob recursively.
-#         if stat.S_ISDIR(f_stat.st_mode):
-#             #file_list += paramiko_glob(f, pattern, sftp)
-#             yield from paramiko_glob(f, pattern, sftp)
-#             #context.log.info(f"####  and {file_list}###")
-#             #print(f"#### and {file_list}###")
-#         # ... if it is a file, check the name pattern and append it to file_list.
-#         elif p.match(f):
-#             file_list.append(f)
-#             #has_files = True
-#             context.log.info(f"#### {f}  and {file_list}###")
-#             print(f"#### {f}  and {file_list}###")
-#             yield RunRequest(
-#                 run_key=f,
-#                 run_config={
-#                     "ops": {"process_file": {"config": {"filename": f}}}
-#                 },
-#             )
-#     #if not has_files:
-#     #    yield SkipReason(f"No files found in {path}.")
-
 def paramiko_glob(path, pattern, sftp):
     """Search recursively for files matching a given pattern.
     
